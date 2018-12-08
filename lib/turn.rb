09cb55b6db1c649811d4f319acb20c7c25dcd3fa
Heloise Taillet <ht2394@columbia.edu>
2018-12-08 01:27:00 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,8 +30,6 @@ def turn(board)
     turn(board)
   end
 end
-
-
 # code your #valid_move? method here
 
 def valid_move?(board, index)
@@ -41,11 +39,10 @@ def valid_move?(board, index)
     true
   elsif position_taken?(board, index) == true
     false
-    else
+  else
     true
   end
 end
-
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
 def position_taken?(board, index)
